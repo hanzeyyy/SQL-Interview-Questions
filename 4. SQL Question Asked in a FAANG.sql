5c049d7300d67efd0bd6d13dcb09c7810bf268f4
hanzeyyy <mhansari1107@gmail.com>
2025-06-25 14:34:12 +0530

    4. SQL Question Asked in a FAANG.sql

--- 4. SQL Question Asked in a FAANG.sql
+++ 4. SQL Question Asked in a FAANG.sql
@@ -1,8 +1,30 @@
 ---SQL Question Asked in a FAANG---
 --write a query to provide the date for nth occurance of sunday in future from given date
+--sunday -> 1
+--monday -> 2
+--tuesday -> 3
+--wednesday -> 4
+--thursday -> 5
+--friday -> 6
+--saturday -> 7
 declare @today_date date;
 declare @n int;
-set @today_date = '2025-06-24';
+set @today_date = '2025-06-25';
 set @n = 3;
 
-select dateadd(week, @n-1, dateadd(day, 8 - datepart(weekday, @today_date), @today_date)) as next_3rd_sunday;
+select dateadd(day, 11 - datepart(weekday, @today_date), @today_date) as upcoming_wednesday;
+
+--we want to see wednesday after 3 weeks
+declare @today_date date;
+declare @n int;
+set @today_date = '2025-06-25';
+set @n = 3;
+
+select dateadd(week, @n-1, dateadd(day, 11 - datepart(weekday, @today_date), @today_date)) as upcoming_3rd_wednesday;
+
+declare @today_date date;
+declare @n int;
+set @today_date = '2025-06-22';
+set @n = 3;
+
+select dateadd(week, @n-1, dateadd(day, 8 - datepart(weekday, @today_date), @today_date)) as upcoming_3rd_sunday;
